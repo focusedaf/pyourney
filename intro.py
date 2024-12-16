d4d579daac1234e6f2fe6a25e4d980287469bddb
focusedaf <126765236+focusedaf@users.noreply.github.com>
2024-12-16 15:25:36 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,18 @@
+print ("hello world")
+print("morpheus is my name", "my name is 19")
+print (32.34)
+
+name = "morpheus"
+age = "19"
+price = 23.44
+print("my name is ",name," and my age is ",age," the price of this candy is ",price)
+
+# identifiers are variable names, symbols that can be used for naming them (0-9,a-z,A-Z,_)
+
+print(type(name))
+print(type(age))
+print(type(price)) 
+
+a = None
+print(type(a))
+print(a)
